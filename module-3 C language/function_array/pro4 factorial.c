b44795c942ce8d189360a5af5f01e46d04508f4b
munaf023 <116436904+munaf023@users.noreply.github.com>
2024-01-20 15:46:25 +0530

    Add files via upload

---
+++ module-3 C language/function_array/pro4 factorial.c
@@ -0,0 +1,13 @@
+//WAP to find factorial using recursion
+#include<stdio.h>
+int factorial(int num)
+{
+	if(num>1)
+	{
+		return num*factorial(num-1);
+	}
+}
+int main()
+{
+	printf("The factorial of %d is %d",num,factorial(5));
+}
---
+++ module-3 C language/function_array/pro7.c
@@ -0,0 +1,15 @@
+// WAP in C to find the length of a string without using library
+//functions.
+//string
+#include<stdio.h>
+int main()
+{
+	int i;
+	char name[]="munaf";
+	for(i=0;name[i] != '\0';i++)
+	{
+		
+	}
+	printf("%d",i);
+	return 0;
+}
